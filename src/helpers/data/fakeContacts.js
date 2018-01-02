7fdbe65a492c10e4447f0f52fbf4f11cd3ff3cd7
Pau Fabregat Pappaterra <pau.fabregat.p@gmail.com>
2018-01-02 22:32:35 +0100

    Fix non-unique uuids

--- src/helpers/data/fakeContacts.js
+++ src/helpers/data/fakeContacts.js
@@ -5,12 +5,12 @@ const fakeContact = [
         image_path: '/assets/trump.jpg'
     },
     {
-        id: 'fa295756-5eb1-4167-b64b-e9763377af85',
+        id: '7b97ed90-313b-4e90-88e0-4bbb03b762c3',
         name: 'Ghandi',
         image_path: '/assets/ghandi.jpg'
     },
     {
-        id: 'fa295756-5eb1-4167-b64b-e9763377af85',
+        id: '207be149-f986-45c6-b38e-354d77b7bd1c',
         name: 'Kendrick',
         image_path: '/assets/kendrick.png'
     }
