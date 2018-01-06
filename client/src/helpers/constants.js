f866fe25ac718ca5e8e5a8e6015eff60ff0700ea
Pau Fabregat Pappaterra <pau.fabregat.p@gmail.com>
2018-01-06 18:58:41 +0100

    Add constants files with events name

---
+++ client/src/helpers/constants.js
@@ -0,0 +1,9 @@
+// SNIPS intent names
+const events = {
+    callContact: 'paupau:callContact',
+    contactOnLine: 'paupau:contactOnLine',
+    stopCall: 'paupau:stopCall'
+};
+
+
+export { events };
