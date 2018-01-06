3040515e8e879e2e69a2d1d07c8dc00d4ae99db3
Pau Fabregat Pappaterra <pau.fabregat.p@gmail.com>
2018-01-06 19:00:10 +0100

    Select contact and redirect
    When call contact event is received, select contact and redict to on
    call page.

--- client/src/components/ContactPage/Contact.js
+++ client/src/components/ContactPage/Contact.js
@@ -2,13 +2,18 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import '../../styles/Contact.css';
 
-export default function ({ contact }) {
+export default function ({ contact, selected }) {
+    const contactStyle = {};
     const imgStyle = {
         background: `url(${contact.image_path}) no-repeat center/cover`
     };
+    if (selected) {
+        contactStyle.backgroundColor = '#896536';
+        contactStyle.color = 'white';
+    }
     return (
         <Link to={`/call/${contact.name}`} style={{ textDecoration: 'none' }}>
-            <div className="contact">
+            <div className="contact" style={contactStyle}>
                 <div className="contact-thumbnail" style={imgStyle} />
                 <div className="contact-name vertical-align"> {contact.name} </div>
             </div>
--- client/src/components/ContactPage/ContactsPage.js
+++ client/src/components/ContactPage/ContactsPage.js
@@ -1,21 +1,54 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+import _ from 'lodash';
 import fakeContact from '../../helpers/data/fakeContacts';
 import Contact from './Contact';
+import { callContact } from '../../helpers/events/events';
 import '../../styles/ContactsPage.css';
 
-export default function () {
-    const contactList = fakeContact.map(contact => (
-        <Contact key={contact.id} contact={contact} />
-    ));
-    return (
-        <div className="contact-list">
-            <div className="contact-list-header vertical-align">
-                Contacts
+class ContactsPage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            contactSelected: null
+        };
+        callContact((err, contact) => {
+            const isContact = fakeContact.some(fkcontact => contact === fkcontact.name);
+            if (isContact) {
+                this.setState({ contactSelected: contact });
+                // redirect to calling screen
+                setTimeout(() =>
+                    this.props.history.push(`/call/${contact}`)
+                    , 3000);
+            }
+        });
+    }
+    render() {
+        const contactList = fakeContact.map((contact) => {
+            let isSelected = false;
+            const contactSelected = this.state.contactSelected;
+            if (!_.isNull(contactSelected) && contactSelected === contact.name) {
+                isSelected = true;
+            }
+            return (
+                <Contact
+                    key={contact.id}
+                    contact={contact}
+                    selected={isSelected}
+                />);
+        });
+        return (
+            <div className="contact-list">
+                <div className="contact-list-header vertical-align">
+                  Contacts
+                </div>
+                <div className="call-to vertical-align">
+                  Call to...
+                </div>
+                {contactList}
             </div>
-            <div className="call-to vertical-align">
-                Call to...
-            </div>
-            {contactList}
-        </div>
-    );
+        );
+    }
 }
+
+export default withRouter(ContactsPage);
