7aa4b0d4ae5d830ffa828a845ab7bb6df007506a
Pau Fabregat Pappaterra <pau.fabregat.p@gmail.com>
2018-01-07 12:24:43 +0100

    Handle server subscription
    Create constants for defined intents and subscribe to those on connect.

---
+++ server/constants.js
@@ -0,0 +1,11 @@
+// SNIPS intent names
+const intentNames = [
+    'paupau:callContact',
+    'paupau:contactOnLine',
+    'paupau:stopCall'
+];
+
+const queueName = 'hermes/intent/';
+
+
+module.exports = { intentNames, queueName };
--- server/server.js
+++ server/server.js
@@ -1,26 +1,32 @@
 const io = require('socket.io')();
 const mqtt = require('mqtt');
-const deviceHost = '192.168.1.40' // TODO change value depending on environment
+const c = require('./constants');
+
+// TODO change value depending on environment
+const deviceHost = '192.168.1.37'
 const mqqtClient  = mqtt.connect(`mqtt://${deviceHost}`);
 
 // Enforced Snips TCP MQTT connection
 mqqtClient.on('connect', function () {
   console.log('Connected to MQTT broker');
-  mqqtClient.subscribe('hermes/intent/callContact');
+  // TODO iterate over queues to subscribe
+  c.intentNames.forEach(intentName => {
+      console.log(`Subscribe to MQTT ${c.queueName}${intentName}`);
+      mqqtClient.subscribe(`${c.queueName}${intentName}`);
+  })
 })
 
 mqqtClient.on('message', (topic, message) => {
   // There is just one user connected per GrandmaPhone device
   console.log(topic, message.toString());
-  io.sockets.emit(topic, message.toString());
+  const intentName = topic.substr(topic.lastIndexOf('/') + 1);
+  io.sockets.emit(intentName, message.toString());
 });
 
 // Socket IO to reach web app
 const port = 8000;
 io.on('connection', (client) => {
-  // here you can start emitting events to the client
   console.log(`Client ${client.id} connected`);
-
 });
 
 io.listen(port);
